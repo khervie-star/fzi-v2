087c08c18670f4193a56ec9e54152831b2872cc0
Kwesi Hervie <herviek2001@gmail.com>
2024-01-09 10:04:17 +0100

    responsveness for donate page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -11,10 +11,10 @@ const Donate = () => {
     <>
       <section className="w-full bg-fz_maroon_light">
         <div className="w-full container mx-auto px-[20px] py-[32px] lg:p-[100px]">
-          <div className="flex justify-between items-center lg:gap-[100px] font-outfit">
+          <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:gap-[100px] font-outfit">
             <div className="flex-1">
               <p className="section_title">Donate</p>
-              <h2 className="text-fz_black text-[56px] font-bold leading-[120%] mb-4">
+              <h2 className="text-fz_black text-[28px] lg:text-[56px] font-bold leading-[1] lg:leading-[120%] mb-4">
                 Making a donation for our children.
               </h2>
               <p className="text-[#1D2130]/70 leading-[160%]">
@@ -33,42 +33,50 @@ const Donate = () => {
       </section>
       <section className="form bg-white">
         <div className="w-full container mx-auto px-[20px] py-[32px] lg:p-[100px]">
-          <div className="flex justify-between items-start lg:gap-[100px] font-outfit">
-            <div className="flex-1">
-              <h2 className="font-bold text-[50px]">How you can contribute</h2>
+          <div className="flex flex-col lg:flex-row lg:justify-between items-start lg:gap-[100px] font-outfit">
+            <div className="flex-1 mb-[50px] lg:mb-0">
+              <h2 className="font-bold text-[28px] lg:text-[50px]">
+                How you can contribute
+              </h2>
 
               <p className="text-fz_text_grey font-light">
                 We accept donations by transfer into our official bank account
               </p>
-              <div className="grid grid-cols-2 gap-6 mt-10">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-10">
                 <div>
-                  <p className="font-bold text-[20px]">Bank Name</p>
+                  <p className="font-bold text-[18px] lg:text-[20px]">
+                    Bank Name
+                  </p>
                   <div className="flex items-center gap-2">
                     <p>Sterling Bank</p>
                     <FaCopy
                       className="text-[#2a952ab1] cursor-pointer"
                       onClick={() => {
                         navigator.clipboard.writeText("Sterling Bank");
-                        toast.success("Bank name copied");
+                        toast.success("Copied to clipboard");
                       }}
                     />
                   </div>
                 </div>
                 <div>
-                  <p className="font-bold text-[20px]">Bank Account Number</p>
+                  <p className="font-bold text-[18px] lg:text-[20px]">
+                    Account Number
+                  </p>
                   <div className="flex items-center gap-2">
                     <p>0501618615</p>
                     <FaCopy
                       className="text-[#2a952ab1] cursor-pointer"
                       onClick={() => {
                         navigator.clipboard.writeText("0501618615");
-                        toast.success("Account number copied");
+                        toast.success("Copied to clipboard");
                       }}
                     />
                   </div>
                 </div>
                 <div>
-                  <p className="font-bold text-[20px]">Account Name</p>
+                  <p className="font-bold text-[18px] lg:text-[20px]">
+                    Account Name
+                  </p>
                   <div className="flex items-center gap-2">
                     <p>Frontida Zois Initiative</p>
                     <FaCopy
@@ -77,20 +85,22 @@ const Donate = () => {
                         navigator.clipboard.writeText(
                           "Frontida Zois Initiative"
                         );
-                        toast.success("Account name copied");
+                        toast.success("Copied to clipboard");
                       }}
                     />
                   </div>
                 </div>
                 <div>
-                  <p className="font-bold text-[20px]">Bank Account Type</p>
+                  <p className="font-bold text-[18px] lg:text-[20px]">
+                    Bank Account Type
+                  </p>
                   <div className="flex items-center gap-2">
                     <p>Current</p>
                     <FaCopy
                       className="text-[#2a952ab1] cursor-pointer"
                       onClick={() => {
                         navigator.clipboard.writeText("Current");
-                        toast.success("Account type copied");
+                        toast.success("Copied to clipboard");
                       }}
                     />
                   </div>
@@ -98,7 +108,9 @@ const Donate = () => {
               </div>
             </div>
             <div className="flex-1">
-              <h2 className="font-bold text-[50px]">What you get</h2>
+              <h2 className="font-bold text-[28px] lg:text-[50px]">
+                What you get
+              </h2>
               <p>A sponsor stands the benefit of the following:</p>
               <ul className="text-fz_text_grey font-nunito_sans flex flex-col gap-4 list-disc list-inside mt-8">
                 <li>Named after a sponsored project.</li>
